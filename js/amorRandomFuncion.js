df70653dc9e6eb25053a08f2f5cfc7d882296d79
Fuyday <Fuyday@users.noreply.github.com>
2022-11-10 11:38:50 -0600

    prueba 2

--- js/amorRandomFuncion.js
+++ js/amorRandomFuncion.js
@@ -1,28 +1,32 @@
-	function amorRandom(){
+	
+var p = document.getElementById("texto");
+
+
+  function amorRandom(){
   var numeroRandom = Math.floor(Math.random()*100);
   
   if(numeroRandom == 50){
-    document.write(numeroRandom + "% no se sabe que pasara.");
+    p.innerText = (numeroRandom + "% no se sabe que pasara.");
   }
   
    if(numeroRandom <= 10){
-    document.write(numeroRandom + "% ni los limones son tan secos.");
+    p.innerText = (numeroRandom + "% ni los limones son tan secos.");
   }
   
   if(numeroRandom <= 49 && numeroRandom >= 11){
-    document.write(numeroRandom + "% hay un poquito de cariño.");
+    p.innerText = (numeroRandom + "% hay un poquito de cariño.");
   }
 
   if(numeroRandom >= 51 && numeroRandom <= 65){
-    document.write(numeroRandom + "% van aumentando las posibilidades siga participando.");
+    p.innerText = (numeroRandom + "% van aumentando las posibilidades siga participando.");
   } 
   
    if(numeroRandom >= 66 && numeroRandom <= 90){
-    document.write(numeroRandom + "% Huele a amor.");
+    p.innerText = (numeroRandom + "% Huele a amor.");
   } 
   
    if(numeroRandom >= 90){
-    document.write(numeroRandom + "% Se viene boda.");
+   p.innerText = (numeroRandom + "% Se viene boda.");
   } 
   
   return numeroRandom;
